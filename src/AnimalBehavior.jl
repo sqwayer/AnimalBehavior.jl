f968a927307e417a6c7bc52e0a449dc16d7e844a
Sami Beaumont <sami.beaumont@yahoo.fr>
2021-09-23 15:39:37 +0200

    forgot to include

--- src/AnimalBehavior.jl
+++ src/AnimalBehavior.jl
@@ -1,7 +1,15 @@
 module AnimalBehavior
+
 using Turing, StructArrays, StatsFuns
 using ForwardDiff: ForwardDiff
 using MacroTools: MacroTools
+
+include("check_types.jl")
+include("macros.jl")
+include("simulate.jl")
+include("inference.jl")
+include("evolutions.jl")
+include("observations.jl")
 
 export @evolution, @observation, simulate, infer 
 end 
